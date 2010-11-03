5c23f935c9da2ef8fe683fec9061bf348d0de714
Schabse Laks <Schabse@GMail.com>
2010-11-03 09:26:39 -0500

    Melave Malka SQL
    [git-p4: depot-paths = "//depot/Shul/Applications/ShomreiTorah.Journal/": change = 2845]

---
+++ MelaveMalka.sql
@@ -0,0 +1,36 @@
+IF schema_id('MelaveMalka') IS NULL
+	EXECUTE('create schema MelaveMalka');
+
+--This table has the same format as a ListMaker list.
+CREATE TABLE MelaveMalka.Invitees (
+	RowId			UNIQUEIDENTIFIER	NOT NULL	ROWGUIDCOL	PRIMARY KEY DEFAULT(newid()),
+	PersonId		UNIQUEIDENTIFIER	NOT NULL	REFERENCES Data.MasterDirectory(Id),
+	[RowVersion]	RowVersion,
+	DateAdded		DATETIME			NOT NULL	DEFAULT getdate(),
+	--Custom fields:
+	[Year]			INTEGER				NOT NULL	DEFAULT year(getdate()),
+	[Source]		NVARCHAR(64)		NOT NULL
+);
+
+CREATE TABLE MelaveMalka.Ads (
+	AdId			UNIQUEIDENTIFIER	NOT NULL	ROWGUIDCOL	PRIMARY KEY DEFAULT(newid()),
+	[Year]			INTEGER				NOT NULL	DEFAULT year(getdate()),
+	DateAdded		DATETIME			NOT NULL	DEFAULT getdate(),
+	AdType			NVARCHAR(64)		NOT NULL,
+	SlideName		VARCHAR(64)			NOT NULL,
+	Comments		NVARCHAR(512)		NULL,
+
+	[RowVersion]	RowVersion
+);
+--This table also has the same format as a ListMaker list.
+CREATE TABLE MelaveMalka.SeatingReservations (
+	RowId			UNIQUEIDENTIFIER	NOT NULL	ROWGUIDCOL	PRIMARY KEY DEFAULT(newid()),
+	PersonId		UNIQUEIDENTIFIER	NOT NULL	REFERENCES Data.MasterDirectory(Id),
+	[RowVersion]	RowVersion,
+	DateAdded		DATETIME			NOT NULL	DEFAULT getdate(),
+	--Custom fields:
+	[Year]			INTEGER				NOT NULL	DEFAULT year(getdate()),
+	--The Seats fields can be null to indicate Not Coming
+	MensSeats		INTEGER				NULL,
+	WomensSeats		INTEGER				NULL
+);
