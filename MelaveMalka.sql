6aa056c125561179f0db163930aab8d49849e113
Schabse Laks <Schabse@GMail.com>
2010-11-04 12:35:15 -0500

    Non-nullable ExternalId
    [git-p4: depot-paths = "//depot/Shul/Applications/ShomreiTorah.Journal/": change = 2874]

--- MelaveMalka.sql
+++ MelaveMalka.sql
@@ -17,7 +17,7 @@ CREATE TABLE MelaveMalka.Ads (
 	[Year]			INTEGER				NOT NULL	DEFAULT year(getdate()),
 	DateAdded		DATETIME			NOT NULL	DEFAULT getdate(),
 	AdType			NVARCHAR(64)		NOT NULL,
-	ExternalId		INTEGER				NULL,
+	ExternalId		INTEGER				NOT NULL,
 	Comments		NVARCHAR(512)		NULL,
 
 	[RowVersion]	RowVersion
