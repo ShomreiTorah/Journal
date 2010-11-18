057a7e262f91eb44c05bc8cab8d9231f47d609ba
Schabse Laks <Schabse@GMail.com>
2010-11-17 20:40:05 -0500

    Reorg
    [git-p4: depot-paths = "//depot/Shul/Applications/ShomreiTorah.Journal/": change = 2972]

--- Forms/ChartsForm.Designer.cs
+++ Forms/ChartsForm.Designer.cs
@@ -24,6 +24,7 @@ namespace ShomreiTorah.Journal.Forms {
 		/// </summary>
 		private void InitializeComponent() {
 			this.components = new System.ComponentModel.Container();
+			DevExpress.XtraCharts.SimpleDiagram simpleDiagram1 = new DevExpress.XtraCharts.SimpleDiagram();
 			DevExpress.XtraCharts.Series series1 = new DevExpress.XtraCharts.Series();
 			DevExpress.XtraCharts.PieSeriesLabel pieSeriesLabel1 = new DevExpress.XtraCharts.PieSeriesLabel();
 			DevExpress.XtraCharts.PiePointOptions piePointOptions1 = new DevExpress.XtraCharts.PiePointOptions();
@@ -31,27 +32,44 @@ namespace ShomreiTorah.Journal.Forms {
 			DevExpress.XtraCharts.PieSeriesView pieSeriesView1 = new DevExpress.XtraCharts.PieSeriesView();
 			DevExpress.XtraCharts.PieSeriesLabel pieSeriesLabel2 = new DevExpress.XtraCharts.PieSeriesLabel();
 			DevExpress.XtraCharts.PieSeriesView pieSeriesView2 = new DevExpress.XtraCharts.PieSeriesView();
+			DevExpress.XtraCharts.Series series2 = new DevExpress.XtraCharts.Series();
+			DevExpress.XtraCharts.PieSeriesLabel pieSeriesLabel3 = new DevExpress.XtraCharts.PieSeriesLabel();
+			DevExpress.XtraCharts.PiePointOptions piePointOptions3 = new DevExpress.XtraCharts.PiePointOptions();
+			DevExpress.XtraCharts.PiePointOptions piePointOptions4 = new DevExpress.XtraCharts.PiePointOptions();
+			DevExpress.XtraCharts.PieSeriesView pieSeriesView3 = new DevExpress.XtraCharts.PieSeriesView();
+			DevExpress.XtraCharts.PieSeriesLabel pieSeriesLabel4 = new DevExpress.XtraCharts.PieSeriesLabel();
+			DevExpress.XtraCharts.PieSeriesView pieSeriesView4 = new DevExpress.XtraCharts.PieSeriesView();
 			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ChartsForm));
-			this.chartBindingSource1 = new ShomreiTorah.Journal.Forms.ChartBindingSource(this.components);
+			this.adTypesSource = new ShomreiTorah.Journal.Forms.ChartBindingSource(this.components);
 			this.xtraTabControl1 = new DevExpress.XtraTab.XtraTabControl();
 			this.xtraTabPage1 = new DevExpress.XtraTab.XtraTabPage();
 			this.chartControl1 = new DevExpress.XtraCharts.ChartControl();
 			this.xtraTabPage2 = new DevExpress.XtraTab.XtraTabPage();
-			((System.ComponentModel.ISupportInitialize)(this.chartBindingSource1)).BeginInit();
+			this.chartControl2 = new DevExpress.XtraCharts.ChartControl();
+			((System.ComponentModel.ISupportInitialize)(this.adTypesSource)).BeginInit();
 			((System.ComponentModel.ISupportInitialize)(this.xtraTabControl1)).BeginInit();
 			this.xtraTabControl1.SuspendLayout();
 			this.xtraTabPage1.SuspendLayout();
 			((System.ComponentModel.ISupportInitialize)(this.chartControl1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(simpleDiagram1)).BeginInit();
 			((System.ComponentModel.ISupportInitialize)(series1)).BeginInit();
 			((System.ComponentModel.ISupportInitialize)(pieSeriesLabel1)).BeginInit();
 			((System.ComponentModel.ISupportInitialize)(pieSeriesView1)).BeginInit();
 			((System.ComponentModel.ISupportInitialize)(pieSeriesLabel2)).BeginInit();
 			((System.ComponentModel.ISupportInitialize)(pieSeriesView2)).BeginInit();
+			this.xtraTabPage2.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.chartControl2)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(series2)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(pieSeriesLabel3)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(pieSeriesView3)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(pieSeriesLabel4)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(pieSeriesView4)).BeginInit();
 			this.SuspendLayout();
 			// 
-			// chartBindingSource1
+			// adTypesSource
 			// 
-			this.chartBindingSource1.Position = 0;
+			this.adTypesSource.DataSet = ShomreiTorah.Journal.Forms.ChartDataSet.AdTypes;
+			this.adTypesSource.Position = 0;
 			// 
 			// xtraTabControl1
 			// 
@@ -75,11 +93,15 @@ namespace ShomreiTorah.Journal.Forms {
 			// 
 			// chartControl1
 			// 
+			this.chartControl1.DataSource = this.adTypesSource;
+			simpleDiagram1.Dimension = 1;
+			this.chartControl1.Diagram = simpleDiagram1;
 			this.chartControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.chartControl1.Legend.AlignmentHorizontal = DevExpress.XtraCharts.LegendAlignmentHorizontal.Right;
 			this.chartControl1.Location = new System.Drawing.Point(0, 0);
 			this.chartControl1.Name = "chartControl1";
-			series1.ArgumentDataMember = "AdTypes.Type";
-			series1.DataSource = this.chartBindingSource1;
+			series1.ArgumentDataMember = "Type";
+			series1.DataSource = this.adTypesSource;
 			pieSeriesLabel1.LineVisible = true;
 			series1.Label = pieSeriesLabel1;
 			piePointOptions1.Pattern = "{A}s: {V}";
@@ -96,7 +118,7 @@ namespace ShomreiTorah.Journal.Forms {
 			piePointOptions2.ValueNumericOptions.Precision = 0;
 			series1.PointOptions = piePointOptions2;
 			series1.SynchronizePointOptions = false;
-			series1.ValueDataMembersSerializable = "AdTypes.Value";
+			series1.ValueDataMembersSerializable = "Value";
 			pieSeriesView1.RuntimeExploding = false;
 			series1.View = pieSeriesView1;
 			this.chartControl1.SeriesSerializable = new DevExpress.XtraCharts.Series[] {
@@ -110,9 +132,42 @@ namespace ShomreiTorah.Journal.Forms {
 			// 
 			// xtraTabPage2
 			// 
+			this.xtraTabPage2.Controls.Add(this.chartControl2);
 			this.xtraTabPage2.Name = "xtraTabPage2";
 			this.xtraTabPage2.Size = new System.Drawing.Size(745, 456);
-			this.xtraTabPage2.Text = "xtraTabPage2";
+			this.xtraTabPage2.Text = "Ad Count by Type";
+			// 
+			// chartControl2
+			// 
+			this.chartControl2.DataSource = this.adTypesSource;
+			this.chartControl2.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.chartControl2.Legend.AlignmentHorizontal = DevExpress.XtraCharts.LegendAlignmentHorizontal.Right;
+			this.chartControl2.Location = new System.Drawing.Point(0, 0);
+			this.chartControl2.Name = "chartControl2";
+			series2.ArgumentDataMember = "Type";
+			series2.DataSource = this.adTypesSource;
+			pieSeriesLabel3.LineVisible = true;
+			series2.Label = pieSeriesLabel3;
+			piePointOptions3.Pattern = "{A}s: {V}";
+			piePointOptions3.PercentOptions.ValueAsPercent = false;
+			piePointOptions3.PointView = DevExpress.XtraCharts.PointView.ArgumentAndValues;
+			series2.LegendPointOptions = piePointOptions3;
+			series2.Name = "Series 1";
+			piePointOptions4.Pattern = "{A}s";
+			piePointOptions4.PointView = DevExpress.XtraCharts.PointView.Argument;
+			series2.PointOptions = piePointOptions4;
+			series2.SynchronizePointOptions = false;
+			series2.ValueDataMembersSerializable = "Count";
+			pieSeriesView3.RuntimeExploding = false;
+			series2.View = pieSeriesView3;
+			this.chartControl2.SeriesSerializable = new DevExpress.XtraCharts.Series[] {
+        series2};
+			pieSeriesLabel4.LineVisible = true;
+			this.chartControl2.SeriesTemplate.Label = pieSeriesLabel4;
+			pieSeriesView4.RuntimeExploding = false;
+			this.chartControl2.SeriesTemplate.View = pieSeriesView4;
+			this.chartControl2.Size = new System.Drawing.Size(745, 456);
+			this.chartControl2.TabIndex = 0;
 			// 
 			// ChartsForm
 			// 
@@ -123,16 +178,24 @@ namespace ShomreiTorah.Journal.Forms {
 			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
 			this.Name = "ChartsForm";
 			this.Text = "Journal Charts";
-			((System.ComponentModel.ISupportInitialize)(this.chartBindingSource1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.adTypesSource)).EndInit();
 			((System.ComponentModel.ISupportInitialize)(this.xtraTabControl1)).EndInit();
 			this.xtraTabControl1.ResumeLayout(false);
 			this.xtraTabPage1.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(simpleDiagram1)).EndInit();
 			((System.ComponentModel.ISupportInitialize)(pieSeriesLabel1)).EndInit();
 			((System.ComponentModel.ISupportInitialize)(pieSeriesView1)).EndInit();
 			((System.ComponentModel.ISupportInitialize)(series1)).EndInit();
 			((System.ComponentModel.ISupportInitialize)(pieSeriesLabel2)).EndInit();
 			((System.ComponentModel.ISupportInitialize)(pieSeriesView2)).EndInit();
 			((System.ComponentModel.ISupportInitialize)(this.chartControl1)).EndInit();
+			this.xtraTabPage2.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(pieSeriesLabel3)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(pieSeriesView3)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(series2)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(pieSeriesLabel4)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(pieSeriesView4)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.chartControl2)).EndInit();
 			this.ResumeLayout(false);
 
 		}
@@ -142,7 +205,8 @@ namespace ShomreiTorah.Journal.Forms {
 		private DevExpress.XtraTab.XtraTabControl xtraTabControl1;
 		private DevExpress.XtraTab.XtraTabPage xtraTabPage1;
 		private DevExpress.XtraTab.XtraTabPage xtraTabPage2;
-		private ChartBindingSource chartBindingSource1;
+		private ChartBindingSource adTypesSource;
 		private DevExpress.XtraCharts.ChartControl chartControl1;
+		private DevExpress.XtraCharts.ChartControl chartControl2;
 	}
 }
--- Forms/ChartsForm.cs
+++ Forms/ChartsForm.cs
@@ -1,22 +1,31 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
-using System.Drawing;
-using System.Text;
 using System.Windows.Forms;
-using DevExpress.XtraEditors;
-using System.Collections;
-using ShomreiTorah.Singularity;
-using ShomreiTorah.Journal.AddIn;
-using System.Threading;
-using ShomreiTorah.Data;
-using DevExpress.XtraTab;
 using DevExpress.XtraCharts;
+using DevExpress.XtraEditors;
+using DevExpress.XtraTab;
+using ShomreiTorah.Data;
+using ShomreiTorah.Journal.AddIn;
+using ShomreiTorah.Singularity;
 
 namespace ShomreiTorah.Journal.Forms {
+	//Charts are bound to LINQ-generated arrays of anonymous types.
+	//These are created by the ChartBindingSource class; it exposes
+	//an enum property to select the datasource.
+	//To make the form load faster, I only bind each chart when its
+	//tab is first focused. (Except at design-time, when everything
+	//will load immediately)  To make this work, ChartBindingSource
+	//will bind to an empty array at runtime until the RefreshList 
+	//method is called by the TabControl's Selected handler.  Until
+	//then, it will contain an empty, strongly-typed array, keeping
+	//the properties for the chart to bind to.  To do this, I pass 
+	//an empty DataContext to the LINQ call.
 	partial class ChartsForm : XtraForm {
-		public ChartsForm() {
+		public ChartsForm() {	//TODO: Year, refresh
 			InitializeComponent();
 		}
 
@@ -25,72 +34,79 @@ namespace ShomreiTorah.Journal.Forms {
 			var chart = e.Page.Controls[0] as ChartControl;
 			if (chart == null) return;
 
-
+			var source = chart.DataSource as ChartBindingSource;
+			Debug.Assert(source != null, e.Page.Text + " chart has no datasource!");
+			if (!source.HasRealData)	//If we haven't loaded this datasource yet, do so.
+				source.RefreshList();
 		}
 	}
 
 	class ChartBindingSource : BindingSource {
-		public ChartBindingSource() { DataSource = new ChartDataSet(); }
-		public ChartBindingSource(IContainer container) : base(container) { DataSource = new ChartDataSet(); }
+		static readonly DataContext dummyContext = new DataContext();
+		static ChartBindingSource() { Program.CheckDesignTime(); Program.CreateTables(dummyContext); }
+		public ChartBindingSource() { }
+		public ChartBindingSource(IContainer container) : base(container) { }
 
 		[Browsable(false)]
 		[DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
-		public new object DataSource {
-			get { return base.DataSource; }
-			set { base.DataSource = value; }
+		public new object DataSource { get { return base.DataSource; } set { base.DataSource = value; } }
+		[Browsable(false)]
+		[DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+		public new string DataMember { get { return base.DataMember; } set { base.DataMember = value; } }
+
+		ChartDataSet dataSet;
+		///<summary>Gets or sets the dataset exposed by the BindingSource.</summary>
+		[Description("Gets or sets the dataset exposed by the BindingSource.")]
+		[Category("Data")]
+		[DefaultValue(ChartDataSet.None)]
+		public ChartDataSet DataSet {
+			get { return dataSet; }
+			set {
+				dataSet = value;
+
+				if (value == ChartDataSet.None)
+					DataSource = null;
+				else if (Program.Current.IsDesignTime)
+					RefreshList();
+				else
+					DataSource = DataSetGenerators[value](dummyContext);
+			}
 		}
-	}
-	class ChartDataSet {
-		static ChartDataSet() { Program.CheckDesignTime(); }
-
-		public ChartData AdTypes { get; private set; }
-
-		public ChartDataSet() {
-			AdTypes = new ChartData(dc =>
-				dc.Table<Pledge>().Rows
-				.Where(p => Names.AdTypes.Any(t => t.PledgeSubType == p.SubType))
-				.GroupBy(
-					p => p.SubType,
-					(subtype, pledges) => new {
-						Type = subtype,
-						Count = pledges.Count(),
-						Value = pledges.Sum(p => p.Amount)
-					}
-				).ToArray()
-			);
-		}
-	}
-
-	class ChartData : IListSource {
-		static readonly Lazy<DataContext> dummyContext = new Lazy<DataContext>(delegate {
-			var context = new DataContext();
-			Program.CreateTables(context);
-			return context;
-		}, LazyThreadSafetyMode.None);
-
-		readonly Func<DataContext, IList> listCreator;
-		public ChartData(Func<DataContext, IList> listCreator) {
-			if (listCreator == null) throw new ArgumentNullException("listCreator");
-			this.listCreator = listCreator;
-
-			if (Program.Current.IsDesignTime) {
-				list = listCreator(Program.Current.DataContext);
-				HasLoaded = true;
-			} else
-				list = listCreator(dummyContext.Value);
-		}
-
-
-		///<summary>Indicates whether the instance is populated with actual data.</summary>
-		public bool HasLoaded { get; private set; }
-
-		IList list;
+		///<summary>Refreshes the datasource from the Singularity DataContext.</summary>
+		///<remarks>This method is called when each tabpage is first selected 
+		///to initially populate the chart, and when the refresh button is clicked.</remarks>
 		public void RefreshList() {
-			list = listCreator(Program.Current.DataContext);
-			HasLoaded = true;
+			if (DataSet == ChartDataSet.None) return;
+			DataSource = DataSetGenerators[DataSet](Program.Current.DataContext);
+			OnListChanged(new ListChangedEventArgs(ListChangedType.Reset, -1));
+			HasRealData = true;
+		}
+		///<summary>Indicates whether the instance is bound to an actual dataset.</summary>
+		[Browsable(false)]
+		public bool HasRealData { get; private set; }
+
+		#region Dataset Generators
+		static IList ReadAdTypes(DataContext dc) {
+			return dc.Table<Pledge>().Rows
+					.Where(p => Names.AdTypes.Any(t => t.PledgeSubType == p.SubType))
+					.GroupBy(
+						p => p.SubType,
+
+						(subtype, pledges) => new {
+							Type = subtype,
+							Count = pledges.Count(),
+							Value = pledges.Sum(p => p.Amount)
+						}
+					).ToArray();
 		}
 
-		public bool ContainsListCollection { get { return false; } }
-		public IList GetList() { return list; }
+		static readonly Dictionary<ChartDataSet, Func<DataContext, IList>> DataSetGenerators = new Dictionary<ChartDataSet, Func<DataContext, IList>> {
+			{ ChartDataSet.AdTypes, ReadAdTypes }
+		};
+		#endregion
+	}
+	enum ChartDataSet {
+		None,
+		AdTypes
 	}
 }
