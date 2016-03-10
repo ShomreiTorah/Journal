cf63ec5cf9c921e9aeaad78ddd18b725911a2040
SLaks <Dev@SLaks.net>
2016-03-10 09:11:02 -0500

    Add format rules by hard-coded people

--- AdFormatter.cs
+++ AdFormatter.cs
@@ -7,6 +7,8 @@ using System.Threading.Tasks;
 using System.Xml.Linq;
 using Microsoft.Office.Core;
 using ShomreiTorah.Common;
+using ShomreiTorah.Data;
+using ShomreiTorah.Journal.AddIn;
 
 namespace ShomreiTorah.Journal {
 	///<summary>Applies preset text formatting to specific substrings in an ad.</summary>
@@ -30,6 +32,21 @@ namespace ShomreiTorah.Journal {
 					return Ad.Row.Pledges.SelectMany(p => AdVerifier.GetNameRegexes(p.Person));
 				case "Match":
 					return new[] { new Regex(element.Attribute("Regex").Value) };
+				case "MatchPerson":
+					foreach (var field in element.Attributes())
+						if (!Person.Schema.Columns.Contains(field.Name.LocalName))
+							throw new ConfigurationException($"Unexpected attribute {field} in <MatchPerson>");
+					var fields = element.Attributes().ToDictionary(
+						a => a.Name.LocalName,
+						a => a.Value
+					);
+					var people = Program.Table<Person>().Rows.Where(p => fields.All(f => f.Value.Equals(p[f.Key])));
+					if (people.Has(2))
+						throw new ConfigurationException($"Format rule {element} matches multiple people: {people.Join(", ", p => p.VeryFullName)}");
+					var person = people.FirstOrDefault();
+					if (person == null)
+						throw new ConfigurationException($"Format rule {element} doesn't match anyone in the master directory.");
+					return AdVerifier.GetNameRegexes(person);
 				case "Format":  // Ignore this element.
 					return Enumerable.Empty<Regex>();
 				default:
