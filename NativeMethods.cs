e1beb7fb0a038ed3786fb19e3ccd24e3ee2579f7
SLaks <Dev@SLaks.net>
2018-05-27 10:01:53 -0400

    Commit active editors when saving

---
+++ NativeMethods.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShomreiTorah.Journal {
+	static class NativeMethods {
+
+		[DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.Winapi)]
+		static extern IntPtr GetFocus();
+
+		public static Control GetFocusedControl() {
+			IntPtr focusedHandle = GetFocus();
+			if (focusedHandle != IntPtr.Zero)
+				return Control.FromHandle(focusedHandle);
+			return null;
+		}
+	}
+}
